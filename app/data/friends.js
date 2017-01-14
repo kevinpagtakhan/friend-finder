b2dc796b37535b22072ffa91f0fce166083899e4
kevinpagtakhan <kevpagtakhan@gmail.com>
2017-01-14 01:41:12 -0800

    Added sample data

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,8 +1,37 @@
-var friendInput = [{
-  "name":"",
-  "photo":"",
-  "scores":[]
-}];
+var friendInput = [
+  {
+    name: "Barack Obama",
+    url: "http://a5.files.biography.com/image/upload/c_fill,cs_srgb,dpr_1.0,g_face,h_300,q_80,w_300/MTE4MDAzNDEwNzg5ODI4MTEw.jpg",
+    scores: [
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3,
+      3
+    ]
+  },
+  {
+    name: "Michelle Obama",
+    url: "http://a2.files.biography.com/image/upload/c_fit,cs_srgb,dpr_1.0,h_1200,q_80,w_1200/MTE5NDg0MDU0ODEyNzIyNzAz.jpg",
+    scores: [
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1,
+      1
+    ]
+  }
+]
 
 var addedFriend = {
     "name":"",
